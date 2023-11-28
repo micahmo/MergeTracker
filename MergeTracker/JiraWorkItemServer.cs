fcaeb3d5a42eb57a4bf8618bb4a5d899af9fbe9e
Micah Morrison <micah.morrison@jci.com>
2023-11-28 12:11:55 -0500

    Update Atlassian API Use the API directly since the NuGet package seems to be broken

--- MergeTracker/JiraWorkItemServer.cs
+++ MergeTracker/JiraWorkItemServer.cs
@@ -1,6 +1,9 @@
 ﻿using System.Diagnostics;
 using Atlassian.Jira;
 using System.Threading.Tasks;
+using Flurl;
+using Flurl.Http;
+using Newtonsoft.Json;
 
 namespace MergeTracker
 {
@@ -15,8 +18,10 @@ namespace MergeTracker
 
         public async Task<string> GetWorkItemUrl(string workItemId)
         {
-            Issue workItem = await GetWorkItem(workItemId);
-            return $"{workItem.Jira.Url}browse/{workItem.Key}";
+            JiraIssue workItem = await GetWorkItem(workItemId);
+            return ServerName
+                .AppendPathSegment("browse")
+                .AppendPathSegment(workItem.Key);
         }
 
         public async Task<string> GetWorkItemTitle(string workItemId)
@@ -24,9 +29,18 @@ namespace MergeTracker
             return (await GetWorkItem(workItemId)).Summary;
         }
 
-        private Task<Issue> GetWorkItem(string workItemId)
+        private async Task<JiraIssue> GetWorkItem(string workItemId)
         {
-            return GetClient().Issues.GetIssueAsync(workItemId);
+            string issue = await ServerName
+                .AppendPathSegment("rest")
+                .AppendPathSegment("api")
+                .AppendPathSegment("2")
+                .AppendPathSegment("issue")
+                .AppendPathSegment(workItemId)
+                .WithHeader("Authorization", $"Basic {RootConfiguration.Instance.JiraPassword}")
+                .GetStringAsync();
+
+            return JsonConvert.DeserializeObject<JiraIssue>(issue);
         }
 
         private Jira GetClient()
@@ -40,5 +54,12 @@ namespace MergeTracker
         }
         private Jira _jiraClient;
         private string _password;
+
+        private class JiraIssue
+        {
+            public string Key { get; set; }
+
+            public string Summary { get; set; }
+        }
     }
 }
